3098d9382cbbf5358bcd35361af30a9349aa9d98
parisc7 <pariscosta4@gmail.com>
2021-11-02 11:35:21 +0200

    Added the Assignment to the repository

---
+++ Assignment 2 Controls/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text loseText;
+    public GameObject completeLevelUI;
+
+    private Rigidbody rb;
+    private int count;
+    private AudioSource asr;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        asr = GetComponent<AudioSource>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        loseText.text = "";
+    }
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+
+        {
+            other.gameObject.SetActive(false);
+            asr.Play();
+            count = count - 1;
+            SetCountText();
+        }
+        else if (other.gameObject.CompareTag("End"))
+        {
+            other.gameObject.SetActive(false);
+            Debug.Log("LEVEL WON!");
+            CompleteLevel();
+        }
+    }
+
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 11)
+        {
+            winText.text = "You Win!";
+        }
+        else if(count<0)
+        {
+            loseText.text = "You Lose!";
+        }
+    }
+}
---
+++ Assignment 2 Controls/Assets/Scripts/Restart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+
+    private static void LoadScene(string v)
+    {
+        throw new NotImplementedException();
+    }
+}
